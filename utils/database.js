104d8ef449465d058813bd2ae0d02ae4ae27dc49
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-17 15:00:37 -0700

    database connection complete

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 const path = require('path');
 const mongoose = require('mongoose');
 const { logger } = require(path.join(__dirname, '../logger/logger'));
 
-mongoose.connect(PROCESS.ENV.LOCAL_CONNECTION, {
-  useNewUrlParser: 'true',
-});
-
-mongoose.connection.on('error', (error) => {
-  logger.error('error', error);
-});
-mongoose.connection.on('connected', (error, res) => {
-  logger.info('database is connected');
-});
+mongoose
+  .connect(process.env.LOCAL_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => logger.info(`🔓 DB connection successful 🔓`))
+  .catch((error) => logger.error(`${error}`));
