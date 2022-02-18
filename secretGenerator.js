2fd7218bf72b9c4a45e8e0af4995bf6c3a42b839
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-18 10:54:38 -0700

    created command to create new jwt secret

--- secretGenerator.js
+++ secretGenerator.js
@@ -1,13 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+const { v4 } = require('uuid');
 const { parse, stringify } = require('envfile');
 const { logger } = require(path.join(__dirname, '/logger/logger'));
 
-fs.readFile('.env', 'utf8', (err, data) => {
+const envFile = '.env';
+
+fs.readFile(envFile, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
+  const secret = v4();
   const parsed = parse(data);
-  logger.info(parsed);
+  parsed.JWT_SECRET = secret;
+  const final = stringify(parsed);
+
+  fs.writeFile(envFile, final, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    logger.info('jwt secret updated');
+  });
 });
