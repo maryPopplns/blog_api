abfa5dc7161cf5d49cfd13bbc50e8e4c3ac08461
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-18 11:29:06 -0700

    added google stat, installed googleoauth2

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require(path.join(__dirname, '../models/user'));
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // [ DEFINE LOCAL STRATEGY ]
 passport.use(
@@ -23,4 +24,29 @@ passport.use(
       });
     });
   })
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/login/google/success',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      const { sub, email, name } = profile._json;
+
+      const query = { email };
+      const update = { fullname: sub, email, username: name };
+      const options = { upsert: true, new: true };
+      // Find the document
+      User.findOneAndUpdate(query, update, options, function (error, result) {
+        if (error) {
+          cb(error);
+        } else {
+          cb(null, result);
+        }
+      });
+    }
+  )
 );
