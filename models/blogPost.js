3bf0aada442333250783d78456934838602179ab
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-21 12:54:33 -0700

    tested blogpost model

--- models/blogPost.js
+++ models/blogPost.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogPostSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   body: { type: String, required: true },
   likes: Number,
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
   date: { type: Date, default: Date.now },
 });
+
+// TODO create vituals for incrementing/decrementing likes
 
 module.exports = mongoose.model('BlogPost', blogPostSchema);
--- utils/playground.js
+++ utils/playground.js
@@ -3,16 +3,17 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 require(path.join(__dirname, '../config/database'));
 const User = require(path.join(__dirname, '../models/user'));
+const BlogPost = require(path.join(__dirname, '../models/blogPost'));
 const { logger } = require(path.join(__dirname, '../logger/logger.js'));
 
 function createUser() {
   bcrypt.genSalt(10, function (error, salt) {
     if (error) {
-      return logger.error('generating salt', error);
+      return logger.error('generating salt error: ', error);
     }
     bcrypt.hash('123', salt, function (error, hash) {
       if (error) {
-        return logger.error('generating hash', error);
+        return logger.error('generating hash error: ', error);
       }
       User.create(
         {
@@ -31,4 +32,30 @@ function createUser() {
     });
   });
 }
+
+async function createBlogPost() {
+  let userID;
+  await User.find()
+    .then((user) => {
+      userID = user;
+    })
+    .catch((error) => logger.error(error));
+  await BlogPost.create(
+    {
+      author: userID[0].id,
+      title: 'this is the title',
+      body: 'this is the body',
+    },
+    function (error, blogPost) {
+      if (error) {
+        mongoose.connection.close();
+        return logger.error(`${error}`);
+      }
+      logger.info('success', `${blogPost}`);
+      mongoose.connection.close();
+    }
+  );
+}
+
 // createUser()
+createBlogPost();
