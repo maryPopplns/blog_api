25e4bc84a5a9e84d85d82fb8e01adfbf9c996127
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-22 11:17:55 -0700

    created route for delete post

--- routes/blog.js
+++ routes/blog.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-const { createPost, updatePost } = require(path.join(
+const { createPost, updatePost, deletePost } = require(path.join(
   __dirname,
   '../controllers/blog'
 ));
 
 router.post('/create', createPost);
 router.post('/:id/update', updatePost);
+router.post('/:id/delete', deletePost);
 
 module.exports = router;
