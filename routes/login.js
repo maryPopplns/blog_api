0e908c911c270a501594ebfa98c9daf4bb80adb5
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-10 09:22:07 -0700

    changed back to get request

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ const {
   login_google_success_get,
 } = require(path.join(__dirname, '../controllers/login'));
 
-router.get('/local', login_local_post);
+router.post('/local', login_local_post);
 router.get('/google', login_google_get);
 router.get('/google/success', login_google_success_get);
 
